ce6f9781e14276926448ea64fb2f1d172565429a
Felix W <felixwillem01@yahoo.com>
2023-08-07 21:56:04 -0400

    added navbar animation

--- personal-project/src/sections/Navbar/Navbar.jsx
+++ personal-project/src/sections/Navbar/Navbar.jsx
@@ -5,74 +5,28 @@ function Navbar() {
   const navRefs = useRef([]);
   navRefs.current = [];
   const [navbar, setNavbar] = useState(false)
-  const [flag, setFlag] = useState(false)
-  let scrl = true;
+
   const addToNavRefs = (ref) => {
     if(ref && !navRefs.current.includes(ref)) {
       navRefs.current.push(ref);
     }
   }
 
-  useEffect(() => {
-  
-  if(window.scrollY >= 80 && navbar !== true) {
-    setNavbar(true);
-  }
-  if(window.scrollY < 90 && navbar !== false) {
-    setNavbar(false)
-  }
-  }, [flag])
-
-
   const handleScroll = () => {
-    if (window.scrollY >= 80) {
-      setFlag(true);
+    if (window.scrollY >= 300) {
+      setNavbar(true);
     } else {
       setNavbar(false);
     }
-    // console.log(window.scrollY)
-    // console.log(navbar)
   }
+  window.addEventListener('scroll', handleScroll)
 
-  if (scrl) {
-    window.addEventListener("scroll", handleScroll)
-    console.log("scroll true")
-    scrl=false
-    console.log(scrl)
-  } else {
-    window.removeEventListener("Scroll", handleScroll)
-    console.log("listener removed")
-  }
-
-  // useEffect(() => {
-  
-  // if(window.scrollY >= 80 && navbar !== true) {
-  //   setNavbar(true);
-  // }
-  // if(window.scrollY < 90 && navbar !== false) {
-  //   setNavbar(false)
-  // }
-  // }, [window.scrollY])
-  
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const changeColor = window.scrollY < 80
-  //   } if (navRefs)
-  //   if(window.scrollY >= changeColor && navbar !== true) {
-  //     setNavbar(true)
-  //   }
-  //   if(window.scrollY < changeColor && navbar !== false) {
-  //     setNavbar(false)
-  //   }
-  //   console.log(window.scrollY)
-  // }, [window.scrollY])
 
     return (
   <>
     <nav className="navbar fixed-top navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        <a className={navbar ? 'navbar-brand nav-home nav-link--scrolled' : 'navbar-brand nav-home' } ref={addToNavRefs} id="home" href="#homepage">Home</a>
+        <a className={navbar ? 'navbar-brand nav-home nav-link--scrolled' : 'navbar-brand nav-home' } id="home" href="#homepage">Home</a>
         <button
           className="navbar-toggler"
           type="button"
@@ -91,21 +45,21 @@ function Navbar() {
           <ul className="navbar-nav ">
 
             <li className="nav-item">
-              <a className="nav-link" id="navs" ref={addToNavRefs} href="#navspage">Skills</a>
+              <a className={navbar ? "nav-link nav-link--scrolled" : "nav-link"} id="navs" href="#navspage">Skills</a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" id="projects" ref={addToNavRefs} href="#projectspage">
+              <a className={navbar ? "nav-link nav-link--scrolled" : "nav-link"} id="projects" href="#projectspage">
               Projects
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" id="about" ref={addToNavRefs} href="#aboutpage">About</a>
+              <a className={navbar ? "nav-link nav-link--scrolled" : "nav-link"} id="about" href="#aboutpage">About</a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" id="contact" ref={addToNavRefs} href="#contactpage">Contact</a>
+              <a className={navbar ? "nav-link nav-link--scrolled" : "nav-link"} id="contact" href="#contactpage">Contact</a>
             </li>
 
           </ul>
