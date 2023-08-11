572131f5e9973bd05eca98d6014a39e9e2031189
Felix W <felixwillem01@yahoo.com>
2023-08-10 23:09:09 -0400

    filled in projectsArray for populating projectCards

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -40,6 +40,69 @@ const projectsArray = [
     github: "movie-forum-app",
     video: "https://www.youtube-nocookie.com/embed/1zzl37IlHMU"
   },
+  {
+  icons: [
+        {
+          src: "/images/html.png",
+          title: "Html",
+          alt: "Html"
+        },
+        {
+          src: "/images/css.png",
+          title: "CSS",
+          alt: "CSS"
+        },
+        {
+          src: "/images/js.png",
+          title: "JavaScript",
+          alt: "JS"
+        },
+        {
+          src: "/images/mapbox.png",
+          title: "Mapbox API",
+          alt: "mapbox"
+        },
+        {
+          src: "/images/google-maps.png",
+          title: "Google Maps API",
+          alt: "google-map",
+        }
+    ],
+    title: "City Snap",
+    description: " Exploration at your fingertips. An interactive App that spins the globe onto the specific geolocation of a city that the user has specified. In addition, the user will be able to view a well-known image from the locale.",
+    live: "https://ilirhajdari.github.io/city-snap/",
+    github: "https://github.com/IlirHajdari/city-snap",
+    video: "https://www.youtube-nocookie.com/embed/a_W2GmhaA_k"
+  },
+  {
+  icons: [
+        {
+          src: "/images/react.png",
+          title: "NodeJS",
+          alt: "NodeJS"
+        },
+        {
+          src: "/images/js.png",
+          title: "JavaScript",
+          alt: "JS"
+        },
+        {
+          src: "/images/html.png",
+          title: "Html",
+          alt: "Html"
+        },
+        {
+          src: "/images/css.png",
+          title: "CSS",
+          alt: "CSS"
+        },
+    ],
+    title: "Personal Portfolio",
+    description: " A personal portfolio website with an elegant and minimalistic design to showcase personal projects and content.",
+    live: "#homepage",
+    github: "https://github.com/FelixW01/Personal-portfolio",
+    video: "https://www.youtube-nocookie.com/embed/AMk6QMj5zTw"
+  }
 ]
 
 
