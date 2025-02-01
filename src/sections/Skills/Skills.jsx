2eb69cd366ff0dfecb815121ab168aaf4593bc32
Felix W <felixwillem01@yahoo.com>
2025-02-01 14:41:13 -0500

    worked on fixing css art bug on spacing

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -57,7 +57,7 @@ function Skills() {
         <>
         <section className="skills-container" id="skillspage">
       <h1 className="skills-header">Skills</h1>
-      <div></div>
+      <div className="skills-content">
       <div className="skills-div">
         <div className="art-container">
           <div className="art-title"><h2>Hover me!</h2></div>
@@ -72,106 +72,127 @@ function Skills() {
           <div id="initial-content"></div>
           {/* <!-- front-end | initial icons --> */}
           <div className={toolHover || yolkHover ? "initial-icons icons-hidden hidden" : "initial-icons hidden"} id="front-end" ref={addToSkillRefs}>
-            <div>
+            <div className="icon-div">
             <img src="/images/react.png" alt="react_icon" />
             <p className="icon-title">React</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/css.png" alt="css_icon" />
             <p className="icon-title">CSS 3</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/html.png" alt="html_icon" />
             <p className="icon-title">HTML 5</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/js.png" alt="js_icon" />
             <p className="icon-title">Javascript</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/bootstrap.png" alt="bootstrap_icon" />
             <p className="icon-title">Bootstrap</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/redux.png" alt="Redux_icon" />
             <p className="icon-title">Redux</p>
+            </div>
+
+            <div className="icon-div">
+            <img src="/images/ant-design.png" alt="Ant-design_icon" />
+            <p className="icon-title">Ant Design</p>
+            </div>
+
+            <div className="icon-div">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" alt="sass-img"/>
+            <p className="icon-title">SASS</p>
             </div>
 
           </div>
 
           {/* <!-- tools icons --> */}
           <div className={toolHover ? "skills-icons icons-reveal hidden": "skills-icons icons-hidden hidden"} id="tools" ref={addToSkillRefs}>
-            <div>
+            <div className="icon-div">
             <img src="/images/git.png" alt="git_icon" />
             <p className="icon-title">Git</p>
-            </div>
+            </div> 
 
-            <div>
+            <div className="icon-div">
             <img src="/images/heroku.png" alt="heroku_icon" />
             <p className="icon-title">Heroku</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/oauth.png" alt="oauth_icon" />
             <p className="icon-title">OAuth</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/jest.png" alt="jest_icon" />
             <p className="icon-title">Jest</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/insomnia.png" alt="insomnia_icon" />
             <p className="icon-title">Insomnia</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg" alt="graphQL_icon" />
             <p className="icon-title">GraphQL</p>
+            </div>
+
+            <div className="icon-div">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" alt="postman_icon" />
+            <p className="icon-title">Postman</p>
+            </div>
+
+            <div className="icon-div">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="github_icon" />
+            <p className="icon-title">GitHub</p>
             </div>
 
           </div>
 
           {/* <!-- back-end --> */}
           <div className={yolkHover ? "skills-icons hidden" : "skills-icons icons-hidden hidden"} id="back-end" ref={addToSkillRefs}>
-            <div>
+            <div className="icon-div">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" alt="nodejs_icon"/>
             <p className="icon-title">Node JS</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="mysql_icon"/>
             <p className="icon-title">MySQL</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-plain.svg" alt="sequelize_icon"/>
             <p className="icon-title">Sequelize</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="mongodb_icon"/>
             <p className="icon-title">MongoDB</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express_icon"/>
             <p className="icon-title">Express</p>
             </div>
 
-            <div>
+            <div className="icon-div">
             <img src="/images/api.png" alt="restApi_icon"/>
             <p className="icon-title">Rest Api</p>
             </div>
 
           </div>  
         </div>
+      </div>
       </div>
     </section>
 
